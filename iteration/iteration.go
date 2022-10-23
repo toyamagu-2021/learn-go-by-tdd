4b3d09cc13e952deae2e26c1bffdb1f0892120f8
toyamagu2021@gmail.com <toyamagu2021@gmail.com>
2022-10-23 20:45:16 +0900

    add var iteration
    Signed-off-by: toyamagu2021@gmail.com <toyamagu2021@gmail.com>

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,8 +1,8 @@
 package iteration
 
-func Repeat(character string) string {
+func Repeat(character string, iteration int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < iteration; i++ {
 		repeated += character
 	}
 	return repeated
