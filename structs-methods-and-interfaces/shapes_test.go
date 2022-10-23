868d18e1550be17f69b6b5c49b27137d101edea7
toyamagu2021@gmail.com <toyamagu2021@gmail.com>
2022-10-24 00:07:29 +0900

    add perimeter
    Signed-off-by: toyamagu2021@gmail.com <toyamagu2021@gmail.com>

--- structs-methods-and-interfaces/shapes_test.go
+++ structs-methods-and-interfaces/shapes_test.go
@@ -5,27 +5,27 @@ import (
 	"testing"
 )
 
-// func TestPerimeter(t *testing.T) {
-// 	areaTests := []struct {
-// 		name         string
-// 		shape        Shape
-// 		hasPerimeter float64
-// 	}{
-// 		{name: "Rectangle", shape: Rectangle{Width: 10, Height: 10}, hasPerimeter: 40.0},
-// 		{name: "Circle", shape: Circle{10}, hasPerimeter: 2 * math.Pi * 10},
-// 		// {name: "Triangle", shape: Triangle{12, 6}, hasPerimeter: ?},
-// 	}
+func TestPerimeter(t *testing.T) {
+	t.Run("Rectangle", func(t *testing.T) {
+		r := Rectangle{Width: 10.0, Height: 10.0}
+		got := r.Perimeter()
+		want := 40.0
 
-// 	for _, tt := range areaTests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			got := tt.shape.Perimeter()
-// 			if got != tt.hasPerimeter {
-// 				t.Errorf("%#v got %.2f want %.2g", tt.shape, got, tt.hasPerimeter)
-// 			}
-// 		})
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
 
-// 	}
-// }
+	t.Run("Circle", func(t *testing.T) {
+		c := Circle{Radius: 10.0}
+		got := c.Perimeter()
+		want := 2 * math.Pi * c.Radius
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+}
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
