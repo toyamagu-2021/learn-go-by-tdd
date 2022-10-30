2a16cb24a84c0a0b136a1705f12badf3936f0057
toyamagu2021@gmail.com <toyamagu2021@gmail.com>
2022-10-30 15:21:39 +0900

    context
    Signed-off-by: toyamagu2021@gmail.com <toyamagu2021@gmail.com>

---
+++ context/context_test.go
@@ -0,0 +1,104 @@
+package context
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+type StubStore struct {
+	response string
+}
+
+func (s *StubStore) Fetch() string {
+	return s.response
+}
+
+type SpyStore struct {
+	response string
+	t        *testing.T
+}
+
+type SpyResponseWriter struct {
+	written bool
+}
+
+func (s *SpyResponseWriter) Header() http.Header {
+	s.written = true
+	return nil
+}
+
+func (s *SpyResponseWriter) Write([]byte) (int, error) {
+	s.written = true
+	return 0, errors.New("not impemented")
+}
+
+func (s *SpyResponseWriter) WriteHeader(statudCode int) {
+	s.written = true
+}
+
+func (s *SpyStore) Fetch(ctx context.Context) (string, error) {
+	data := make(chan string, 1)
+	go func() {
+		var result string
+		for _, c := range s.response {
+			select {
+			case <-ctx.Done():
+				s.t.Log("spy store get cancelled")
+				return
+			default:
+				time.Sleep(10 * time.Millisecond)
+				result += string(c)
+			}
+		}
+		data <- result
+	}()
+
+	select {
+	case <-ctx.Done():
+		return "", ctx.Err()
+	case res := <-data:
+		return res, nil
+	}
+}
+
+func TestHandler(t *testing.T) {
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+		data := "hello, world"
+		store := &SpyStore{response: data, t: t}
+		svr := Server(store)
+
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		cancellingCtx, cancel := context.WithCancel(req.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		req = req.WithContext(cancellingCtx)
+
+		res := &SpyResponseWriter{}
+
+		svr.ServeHTTP(res, req)
+
+		if res.written {
+			t.Error("a response should not have been written")
+		}
+	})
+
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+		data := "hello, world"
+		store := &SpyStore{response: data, t: t}
+		svr := Server(store)
+
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		res := httptest.NewRecorder()
+
+		svr.ServeHTTP(res, req)
+
+		if res.Body.String() != data {
+			t.Errorf(`got "%s", want "%s"`, res.Body.String(), data)
+		}
+	})
+
+}
